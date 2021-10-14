9071e7adf847969827e630f795cf86bb357b17a3
Erik <erik@understandling.com>
2021-10-14 11:17:56 +0200

    Try again

--- retentioneering/core/core_functions/get_clusters.py
+++ retentioneering/core/core_functions/get_clusters.py
@@ -74,10 +74,10 @@ def get_clusters(self, *,
                                          ngram_range=ngram_range)
 
     # Remove features that occur infrequently
-    #features = features.loc[:, features.astype(bool).sum(axis=0) >= min_feature_count]
+    features = features.loc[:, features.astype(bool).sum(axis=0) >= min_feature_count]
     # Add custom features back in
-    #if type(custom_features) != 'str':
-        #features = features.join(custom_features)
+    if type(custom_features) != 'str':
+        features = features.join(custom_features)
 
     # obtain clusters
     if not hasattr(self, 'clusters') or refit_cluster:
@@ -90,6 +90,7 @@ def get_clusters(self, *,
     # init and obtain bool vector for targets:
     targets_bool = [np.array([False] * len(self.clusters))]
     target_names = [' ']
+    print(targets)
 
     if targets is not None:
         targets_bool = []
